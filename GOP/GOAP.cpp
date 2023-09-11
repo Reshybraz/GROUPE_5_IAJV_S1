ff116e3996ae21b66887b8db90059d84d5748b3e
Reshybraz <59066353+Reshybraz@users.noreply.github.com>
2023-09-11 12:40:51 +0200

    Base Class

--- GOP/GOAP.cpp
+++ GOP/GOAP.cpp
@@ -1,4 +1,4 @@
-// GOP.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
+// GOAP.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
 //
 
 #include <iostream>
---
+++ GOP/State.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include <string>
+#ifndef _STATE_HPP_
+#define _STATE_HPP_
+
+class State{
+private:
+	std::string name;
+public:
+
+};
+
+#endif
